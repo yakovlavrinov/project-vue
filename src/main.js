1f0d7839e5d5b758397f16ca98934fb7336659cd
Yakov Lavrinov <lawrinov.yackov@yandex.ru>
2025-08-26 21:19:05 +0300

    add directives

--- src/main.js
+++ src/main.js
@@ -5,10 +5,16 @@ import App from "./App";
 import components from "./components/UI/index";
 import router from "./router/router";
 
+import directives from "./directives";
+
 const app = createApp(App);
 
 components.forEach((component) => {
   app.component(component.name, component);
 });
+directives.forEach((directive) => {
+
+  app.directive(directive.name, directive)
+})
 
 app.use(router).mount("#app");
--- src/pages/PostPage.vue
+++ src/pages/PostPage.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="app">
     <h1>Страница с постами</h1>
-    <my-input v-model="searchQuery" placeholder="Поиск..." />
+    <my-input v-focus v-model="searchQuery" placeholder="Поиск..." />
     <div class="app__btns">
       <my-button @click="showDialog">Создать пост</my-button>
       <my-select v-model="selectedSort" :options="sortOptions" />
@@ -15,7 +15,7 @@
       @remove="removePost"
     />
     <div v-else>Идет загрузка...</div>
-    <div ref="observer" class="observer"></div>
+    <div v-intersection="loadMorePosts" class="observer"></div>
     <!-- <div class="page__wrapper">
       <div
         v-for="pageNumber in totalPages"
@@ -124,19 +124,7 @@ export default {
   },
   mounted() {
     this.fetchPosts();
-    const options = {
-      rootMargin: '0px',
-      threshold: 1.0
-    }
-    const callback = (entries, observer) => {
-      if (entries[0].isIntersecting && this.page < this.totalPages) {
-        this.loadMorePosts()
-        console.log('пересечение')
-      }
-    }
-
-    const observer = new IntersectionObserver(callback, options)
-    observer.observe(this.$refs.observer)
+   
   },
   computed: {
     sortedPost() {
